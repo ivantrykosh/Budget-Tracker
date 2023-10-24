870c199f1a73898869ae61c3237e39157dec64ae
Ivan Trykosh <trykoshivan4@gmail.com>
2023-10-24 17:20:55 +0300

    Made FABs for adding incomes and expenses and their animation

--- client/app/src/main/java/com/ivantrykosh/app/budgettracker/client/MainActivity.kt
+++ client/app/src/main/java/com/ivantrykosh/app/budgettracker/client/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ivantrykosh.app.budgettracker.client
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ivantrykosh.app.budgettracker.client.adapter.OperationItemAdapter
 import com.ivantrykosh.app.budgettracker.client.data.Datasource
 import com.ivantrykosh.app.budgettracker.client.databinding.ActivityMainBinding
@@ -12,8 +14,21 @@ import java.util.Currency
 import java.util.Date
 import java.util.Locale
 
+
 class MainActivity : AppCompatActivity() {
+    // Value for binding activity_main.xml
     private lateinit var binding: ActivityMainBinding
+
+    // 45 degree clockwise rotation animation
+    private val rotateClockWise: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_clock_wise)
+    }
+
+    // 45 degree anti clockwise rotation animation
+    private val rotateAntiClockWise: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_anti_clock_wise)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,18 +41,24 @@ class MainActivity : AppCompatActivity() {
 
         // Set on click listener for main FAB
         binding.mainFloatingActionButton.setOnClickListener {
-            Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+            binding.mainFloatingActionButton.isExpanded = !binding.mainFloatingActionButton.isExpanded
+            if (binding.mainFloatingActionButton.isExpanded) {
+                binding.mainFloatingActionButton.startAnimation(rotateClockWise)
+            } else {
+                binding.mainFloatingActionButton.startAnimation(rotateAntiClockWise)
+            }
         }
 
         // Set text for balance
         val stringDate = SimpleDateFormat("MMM yyyy", Locale.getDefault()).format(Date())
         binding.textViewBalanceFor.text = getString(R.string.balance_for, stringDate)
 
-        // Set text for incomes, expenses and total values
+        // Set format for numbers
         val format = NumberFormat.getCurrencyInstance()
         format.maximumFractionDigits = 2
         format.currency = Currency.getInstance("USD")
 
+        // Set text for incomes, expenses and total values
         val myDatasetBalanceOverview = Datasource().loadBalanceOverview()
         binding.mainIncomesValue.text = format.format(myDatasetBalanceOverview.incomes)
         binding.mainExpensesValue.text = format.format(myDatasetBalanceOverview.expenses)
@@ -52,7 +73,5 @@ class MainActivity : AppCompatActivity() {
         val myDatasetExpenses = Datasource().loadOperations()
         binding.lastExpensesRecyclerView.adapter = OperationItemAdapter(this, myDatasetExpenses, 2)
         binding.lastExpensesRecyclerView.setHasFixedSize(true)
-
-//        TODO("Add FAB button for extensible FABs (add income and add expense)")
     }
 }
