3a112fd1cdbe4b693c48f5063e14d27744e2fa32
Ivan Trykosh <trykoshivan4@gmail.com>
2023-10-28 12:36:12 +0300

    initial server commit

---
+++ server/src/main/java/com/ivantrykosh/app/budgettracker/server/BudgetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ivantrykosh.app.budgettracker.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ivantrykosh/app/budgettracker/server/BudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivantrykosh.app.budgettracker.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
