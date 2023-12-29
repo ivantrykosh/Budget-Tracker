81bf5123fd359d20631312bdde07d87ffc27273b
Ivan Trykosh <trykoshivan4@gmail.com>
2023-12-30 00:08:10 +0200

    Update build.gradle.kts

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -3,12 +3,4 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("org.sonarqube") version "4.4.1.3373"
-}
-sonar {
-  properties {
-    property("sonar.projectKey", "ivantrykosh_Budget-Tracker")
-    property("sonar.organization", "ivantrykosh")
-    property("sonar.host.url", "https://sonarcloud.io")
-  }
 }
